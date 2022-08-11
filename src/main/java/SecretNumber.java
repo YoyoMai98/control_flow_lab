c603ebe8e03c4ace834870943bcab2cc2b7bc280
Yongran <maiyongran@gmail.com>
2022-08-11 15:24:18 +0100

    create a secret number

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -1,2 +1,8 @@
 public class SecretNumber {
+    public static void main(String[] args) {
+//        create the secret number
+        System.out.println("What is the secret number?");
+        int secretNumber = 23;
+        
+    }
 }
