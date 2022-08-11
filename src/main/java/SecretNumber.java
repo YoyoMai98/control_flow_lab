6ada27b8f7ec0a346198900c83a4a741592ec97e
Yongran <maiyongran@gmail.com>
2022-08-11 15:32:08 +0100

    collect user input

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class SecretNumber {
     public static void main(String[] args) {
 //        create the secret number
         System.out.println("What is the secret number?");
         int secretNumber = 23;
-        
+
+        //        collect user input
+        Scanner reader = new Scanner(System.in);
+        String guessedInput = reader.nextLine();
+        int guessedNumber = Integer.parseInt(guessedInput);
     }
 }
