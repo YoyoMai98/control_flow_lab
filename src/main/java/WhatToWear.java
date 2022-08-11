b2a200afc5acfe04f2513b55543da3f9ea74a167
Yongran <maiyongran@gmail.com>
2022-08-11 15:55:05 +0100

    collect user weather and temperature

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class WhatToWear {
+    public static void main(String[] args) {
+//        collect weather input
+        System.out.println("What is the weather?");
+        Scanner readWeather = new Scanner(System.in);
+        String weather = readWeather.nextLine();
+//        collect average temperature input
+        System.out.println("What is the temperature?");
+        Scanner readTemp = new Scanner(System.in);
+        String temperatureInput = readTemp.nextLine();
+        int temperature = Integer.parseInt(temperatureInput);
+
+    }
 }
